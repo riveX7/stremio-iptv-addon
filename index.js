bda834f87470a7c70ab2b707aaff8cc1f1ca2611
riveX7 <90065751+riveX7@users.noreply.github.com>
2025-07-04 17:49:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const NodeCache = require('node-cache');
 
 // Config
-const M3U_URL = 'https://raw.githubusercontent.com/Free-TV/IPTV/refs/heads/master/lists/portugal.md';
+const M3U_URL = 'https://raw.githubusercontent.com/LITUATUI/M3UPT/main/M3U/M3UPT.m3u';
 const PORT = process.env.PORT || 8080;
 const FETCH_INTERVAL = parseInt(process.env.FETCH_INTERVAL) || 86400000; // 1 dia
 const FETCH_TIMEOUT = parseInt(process.env.FETCH_TIMEOUT) || 10000;
